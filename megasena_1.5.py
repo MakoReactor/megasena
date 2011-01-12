da0ab2becc4833879d034fcce07f2976f8677a03
MakoReactor <douglas.makubex@gmail.com>
2011-01-12 10:18:46 -0200

    Separado o controle de Versão do Programa para o VERSION

--- megasena_1.5.py
+++ megasena_1.5.py
@@ -4,8 +4,6 @@ import random
 import os
 import time
 
-
-
 #autor
 #Douglas Barbosa dos Santos
 #douglas.makubex@gmail.com
@@ -14,49 +12,7 @@ import time
 #Elias
 
 # Controle da Versão: Version "1.5"
-#
-# version 0.1: 03/01/2011 - Sorteia 6 dezenas aleatoriamente um número de 
-#cada lista. Considerar 00 como sendo o número 60.
 
-# version 0.2: 05/01/2011 - Escolhe o número de dezenas que quer jogar.
-
-# version 0.3: 05/01/2011 - Adcionado menu2 para perguntar se quer
-# continuar jogando
-
-# version 0.4 - 05/01/2011 -  BUG(RESOLVIDO) - Se colocar letras na 
-# primeira pergunta ao invés de numeros na hora em que o programa 
-# pedir para sair vai fazer a pergunta
-# o mesmo número de vezes que errou na primeira pergunta
-
-# version 0.5 05/01/2011 - Resovido bug que dava erro quando se escolhia
-# sair do programa após ter digitado uma opçẽo errada.
-
-# version 0.6 06/01/2011 - Mudado a forma de escolher o numero aleatório.
-# Acrecentado o método random.choice() que escolhe aleatóriamente um
-# número no range - Foi colocada a trava que impde jogos menores que 6
-# dezenas ou maiores que 15 Dezenas. - O programa mostra a aposta com as
-# Dezenas ordenadas
-
-# Versão 1.0 06/01/2011 - Todos os Bugs resolvidos até a versão 0.6.
-
-# Versão 1.1 06/01/2011 - Acrescentado a opção de quantos jogos fazer
-# de uma unica vez.
-
-# Versão 1.1-1 BETA 06/01/2011 - Melhora na saida dos numeros na tela
-
-# Versão 1.4-Beta - 10/01/2011 - Agora o programa salva em um arquivo 
-# na mesma pasta que o programa esta e pergunta se quer imprimir.
-# Usa o comando lpr do Linux para imprimir. 
-
-# Versão 1.4.1-Beta - 11/01/2011 - Programa salva arquivo com nome baseado
-# na hora e data de criação do arquivo. Função nomeArquivo() - pode-se 
-# formatar de varias maneiras.
-
-# Versão 1.5 - 12/01/2011- Programa sem bugs. As mesmas caracteristicas da versão
-# beta
-
-
-# TODO - implementar funcionalidade de impresão também no windows.
 versao = '1.5'
 def nomeArquivo():
 	ano=str(time.localtime()[0])
